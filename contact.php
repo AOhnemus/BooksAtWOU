1fb0b364bcdf05a00ae4469417a91ef5dafe75c9
broehrig17 <49664841+broehrig17@users.noreply.github.com>
2019-05-23 13:54:50 -0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -81,8 +81,6 @@ Thank You For Your Comment! A Member of Our Awesome Team Will Respond to You Wit
     <ul>
 	  <li><a href="index.html">Home</a></li>
       <li><a href="testform.html">Make New Listing</a></li>
-      <li><a href="#">Search for Books</a></li>
-      <li><a href="#">FAQ</a></li>
       <li><a href="contact.html">Contact Us</a></li>
     </ul>
   </nav> 

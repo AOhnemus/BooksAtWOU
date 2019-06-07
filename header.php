d94e8a83812e50a4a47da79bc32d594369c9a5bd
Ubuntu <ubuntu@ip-172-31-47-62.us-east-2.compute.internal>
2019-06-07 04:46:15 +0000

    Tried to fix header formatting.

--- header.php
+++ header.php
@@ -30,13 +30,16 @@ https://www.youtube.com/watch?v=LC9GaXkdxF8-->
 <section>
   <nav>
 	<div class="topnav">
-  <a class="active" a href="index.php">Home</a>
+  <a href="index.php">Home</a>
   <?php
     if (isset($_SESSION['userId'])) {
       echo '<a href="bookform.php">Make New Listing</a>';
     }
-  ?>
+?>
+
   <a href="contactUs.php">Contact Us</a>
+<a class="advanced" a href="searchform.php">Advanced Search</a>
+
   <div class="search-container">
   <form action="searchbar.php" method="post" >
   <input class="searchbar" name='searchbar' type="text" placeholder="Search..">
@@ -48,17 +51,16 @@ https://www.youtube.com/watch?v=LC9GaXkdxF8-->
   <button type="submit"><i class="fa fa-search"></i></button>
   </form>
 </div>
-<a class="advanced" a href="searchform.php">Advanced Search</a>
 	<?php
 		if (isset($_SESSION['userId'])) {
 			echo '<form action="logoutHandle.php" method="post">
 			<button type="submit" name="logoutSubmit">Logout</button></form>';
 		} else {
-			echo '<form action="loginHandle.php" method="post">
+			echo '<a href="signup.php">Signup</a> <form action="loginHandle.php" method="post">
 			<input type="text" name="mailuid" placeholder="Username/E-mail">
 			<input type="password" name="pwd" placeholder="Password">
 			<button type="submit" name="loginSubmit">Login</button>
-			</form><a href="signup.php">Signup</a>';
+			</form>';
 		}
 	?>
 </div>
